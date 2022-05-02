0f9cbbfe9b8457941d50ed04595227bed6e5f2d0
Ardian Luma <ardian.luma@tarmac.io>
2022-05-02 20:02:44 +0200

    🚀: Initial commit, add script and readme

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # aws-auto-sg
-Automatically add/update ingress rule in security group 
+```   
+      __          _______                 _        _____  _____ 
+     /\ \        / / ____|     /\        | |      / ____|/ ____|
+    /  \ \  /\  / / (___      /  \  _   _| |_ ___| (___ | |  __ 
+   / /\ \ \/  \/ / \___ \    / /\ \| | | | __/ _ \\___ \| | |_ |
+  / ____ \  /\  /  ____) |  / ____ \ |_| | || (_) |___) | |__| |
+ /_/    \_\/  \/  |_____/  /_/    \_\__,_|\__\___/_____/ \_____|
+
+ ```
+
+Automatically add/update ingress rule in AWS security group
+
+# 🚩 Pre-requisites
+
+ - awscli (if not, install [here](https://aws.amazon.com/cli/) and configure aws profile)
+ - curl (`apt-get install curl` / `brew install curl`)
+
+# ⚙️ Setup
+
+Clone the repo 
+`git clone https://github.com/arddluma/aws-auto-sg.git`
+
+You have to have properly install awscli with valid credentials!
+Edit `magic.sh` as following:
+- line 3 `SG_ID` add correct security group rule ID `sg-xxxxxxxxxxxxxx`.
+- line 4 `SG_DESCRIPTION` add proper security group rule description. e.g `Office Access port 22`
+  ***(Note description has to be unique because script queries based on description set!)***
+- line 5 `AWS_PROFILE` add AWS profile configured.
+- line 6 `PORT` set port you want to whitelist e.g `22`.
+
+# 🚀 Usage
+
+```
+    chmod +x magic.sh
+    ./magic.sh
+```
+
+# 📖 What is covered
+
+- First time applying the rule
+- If your IP changes, it will update the rule with new IP
+- If your IP has not changed and you try to re-create same rule it shows that rule exist and IP is  already whitelisted.
+
+# ❤️ Contribution
+
+Feel free to fork the repo and create a PR!
---
+++ magic.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+SG_ID="__SECURITY_GROUP_ID__"
+SG_DESCRIPTION="__SECURITY_GROUP_RULE_DESCRIPTION__"
+AWS_PROFILE="__AWS_PROFILE__"
+PORT=22
+AWS_CLI_PATH=$(command -v aws)
+CURL_PATH=$(command -v curl)
+
+cat << "EOF"
+      __          _______                 _        _____  _____ 
+     /\ \        / / ____|     /\        | |      / ____|/ ____|
+    /  \ \  /\  / / (___      /  \  _   _| |_ ___| (___ | |  __ 
+   / /\ \ \/  \/ / \___ \    / /\ \| | | | __/ _ \\___ \| | |_ |
+  / ____ \  /\  /  ____) |  / ____ \ |_| | || (_) |___) | |__| |
+ /_/    \_\/  \/  |_____/  /_/    \_\__,_|\__\___/_____/ \_____|
+
+                           by: Ardd
+
+EOF
+
+
+GET_IP=$(curl -s https://checkip.amazonaws.com)
+GET_IP_CIDR=$GET_IP/32
+
+params () {
+    echo "----------------------------------------------------"
+    printf "AWS PROFILE: $AWS_PROFILE\n"
+    printf "Your IP: $GET_IP_CIDR\n"
+    printf "Security Rule Description: $SG_DESCRIPTION\n"
+    printf "Security Group ID: $SG_ID\n"
+    printf "Port set: $PORT\n"
+    echo -e "----------------------------------------------------\n"
+}
+
+main () {
+    OLD_CIDR=$(aws ec2 describe-security-groups --group-ids $SG_ID --query "SecurityGroups[*].IpPermissions[*].IpRanges[?Description=='$SG_DESCRIPTION'].CidrIp" --output text --profile $AWS_PROFILE)
+
+    if [[ $OLD_CIDR != "" ]] && [[ $OLD_CIDR != $GET_IP_CIDR ]]; then
+        printf "New IP $GET_IP_CIDR is not equal with old IP set in SG $SG_ID - $OLD_CIDR \nRevoking old rule now...\n"
+        aws ec2 revoke-security-group-ingress --group-id $SG_ID --protocol tcp --port $PORT --cidr $OLD_CIDR --profile $AWS_PROFILE > /dev/null 2>&1
+        printf "Old rule is revoked!\n\nSetting up new rule with Source IP: $GET_IP_CIDR for port $PORT"
+    fi
+
+    if [[ $GET_IP != "" ]] && [[ $OLD_CIDR != $GET_IP_CIDR ]]; then
+        aws ec2 authorize-security-group-ingress --group-id $SG_ID --ip-permissions IpProtocol=tcp,FromPort=$PORT,ToPort=$PORT,IpRanges="[{CidrIp=$GET_IP_CIDR,Description='$SG_DESCRIPTION'}"] --profile $AWS_PROFILE > /dev/null 2>&1
+        printf "\nSG rule is applied successfully! \n"
+    fi
+
+    if [[ $GET_IP_CIDR == $OLD_CIDR ]]; then
+        printf "Your IP $GET_IP_CIDR is already whitelisted !\n"
+    fi
+}
+if [[ ! -x $AWS_CLI_PATH || ! -x $CURL_PATH ]]; then
+    echo "You do not have aws cli or curl installed !" 
+    exit 0
+else
+    params
+    echo -e "Everything set! \nStarting now..."
+    main
+fi
